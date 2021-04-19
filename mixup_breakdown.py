9374d4feb4a126457dcb5a2767d8414482873e16
s_kuchuganova <s.kuchuganova@g.nsu.ru>
2021-04-19 19:44:29 +0700

    add code

---
+++ mixup_breakdown.py
@@ -0,0 +1,23 @@
+def Break(l, teacher_pred, mixture_lenghts):
+    
+    M,N = teacher_pred.shape[2], teacher_pred.shape[3]
+    
+    flat_estimate = remove_pad_and_flat(teacher_pred.data, mixture_lengths)
+    
+    s1 = flat_estimate[0][0]
+    s2 = flat_estimate[0][1]  
+    
+    s1 = l*s1
+    s2 = (1-l)*s2
+    
+    sources_arr = []
+    sources_arr.append(np.array(torch.reshape(torch.Tensor(s1),(1,M,N)))) #собираем 2 сигнала в 1 для pad source
+    sources_arr.append(np.array(torch.reshape(torch.Tensor(s2),(1, M,N))))
+    pad_value = 0
+    sources_pad = pad_list([torch.from_numpy(s).float() for s in sources_arr], pad_value)
+
+    new_padded_source = sources_pad.permute((1, 0, 2, 3)).contiguous()
+    return new_padded_source
+
+def Mix(l,s1,s2):
+    return l*s1+(1-l)*s2
---
+++ train_utils.py
@@ -0,0 +1,28 @@
+
+
+
+def update_ema_variables(model, ema_model, alpha, global_step):
+    # Use the true average until the exponential average is more correct
+    alpha = min(1 - 1 / (global_step + 1), alpha)
+    for ema_param, param in zip(ema_model.parameters(), model.parameters()):
+        ema_param.data.mul_(alpha).add_(1 - alpha, param.data)
+
+
+def remove_pad_and_flat(inputs, inputs_lengths):
+    """
+    Args:
+        inputs: torch.Tensor, [B, C, K, L] or [B, K, L]
+        inputs_lengths: torch.Tensor, [B]
+    Returns:
+        results: a list containing B items, each item is [C, T], T varies
+    """
+    results = []
+    dim = inputs.dim()
+    if dim == 4:
+        C = inputs.size(1)
+    for input, length in zip(inputs, inputs_lengths):
+        if dim == 4: # [B, C, K, L]
+            results.append(input[:,:length].view(C, -1).cpu().numpy())
+        elif dim == 3:  # [B, K, L]
+            results.append(input[:length].view(-1).cpu().numpy())
+    return results
